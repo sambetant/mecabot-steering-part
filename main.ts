dd86f0fb242ea7c3fe65d9df93caf3364d3bca5a
sambetant <104916257+sambetant@users.noreply.github.com>
2023-08-15 21:32:15 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,34 +1,59 @@
-radio.setGroup(1)
+radio.setGroup(2)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        if (input.acceleration(Dimension.Y) < -200) {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire1)) {
+        if (input.acceleration(Dimension.Y) < -200 && Math.abs(input.acceleration(Dimension.X)) < 100) {
             basic.clearScreen()
             led.plot(2, 0)
             radio.sendNumber(1)
         }
-        if (input.acceleration(Dimension.Y) > 200) {
+        if (input.acceleration(Dimension.Y) > 200 && Math.abs(input.acceleration(Dimension.X)) < 100) {
             basic.clearScreen()
             led.plot(2, 4)
             radio.sendNumber(2)
         }
-        if (input.acceleration(Dimension.X) > 200) {
+        if (input.acceleration(Dimension.X) > 200 && Math.abs(input.acceleration(Dimension.Y)) < 100) {
             basic.clearScreen()
             led.plot(4, 2)
             radio.sendNumber(3)
         }
-        if (input.acceleration(Dimension.X) < -200) {
+        if (input.acceleration(Dimension.X) < -200 && Math.abs(input.acceleration(Dimension.Y)) < 100) {
             basic.clearScreen()
             led.plot(0, 2)
             radio.sendNumber(4)
         }
+        if (input.acceleration(Dimension.X) < -200 && input.acceleration(Dimension.Y) > 200) {
+            basic.clearScreen()
+            led.plot(0, 4)
+            radio.sendNumber(5)
+        }
+        if (input.acceleration(Dimension.X) > 200 && input.acceleration(Dimension.Y) < -200) {
+            basic.clearScreen()
+            led.plot(4, 0)
+            radio.sendNumber(6)
+        }
+        if (input.acceleration(Dimension.X) < -200 && input.acceleration(Dimension.Y) < -200) {
+            basic.clearScreen()
+            led.plot(0, 0)
+            radio.sendNumber(7)
+        }
+        if (input.acceleration(Dimension.X) > 200 && input.acceleration(Dimension.Y) > 200) {
+            basic.clearScreen()
+            led.plot(4, 4)
+            radio.sendNumber(8)
+        }
+        if (Math.abs(input.acceleration(Dimension.X)) < 50 && Math.abs(input.acceleration(Dimension.Y)) < 50) {
+            basic.clearScreen()
+            led.plot(2, 2)
+            radio.sendNumber(0)
+        }
     } else {
         basic.clearScreen()
         basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
             `)
         radio.sendNumber(0)
     }
