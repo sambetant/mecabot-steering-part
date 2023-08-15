9bf7775d7a50ae6e38706881aa18fc6b5450ee31
sambetant <104916257+sambetant@users.noreply.github.com>
2023-08-15 07:50:29 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,35 @@
-let HoekX = 0
 radio.setGroup(1)
 basic.forever(function () {
-    HoekX = input.acceleration(Dimension.X)
-    basic.showNumber(HoekX)
+    if (input.buttonIsPressed(Button.A)) {
+        if (input.acceleration(Dimension.Y) < -200) {
+            basic.clearScreen()
+            led.plot(2, 0)
+            radio.sendNumber(1)
+        }
+        if (input.acceleration(Dimension.Y) > 200) {
+            basic.clearScreen()
+            led.plot(2, 4)
+            radio.sendNumber(2)
+        }
+        if (input.acceleration(Dimension.X) > 200) {
+            basic.clearScreen()
+            led.plot(4, 2)
+            radio.sendNumber(3)
+        }
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.clearScreen()
+            led.plot(0, 2)
+            radio.sendNumber(4)
+        }
+    } else {
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        radio.sendNumber(0)
+    }
 })
